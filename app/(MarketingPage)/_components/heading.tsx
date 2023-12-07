b8c1260be06ed7d15e3705c101a7d298c4754485
wasd1g <koki.karadjov@gmail.com>
2023-12-08 01:10:14 +0200

    nomer 1 change

---
+++ app/(MarketingPage)/_components/heading.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export const Heading = () => {
+    return (
+        <div className="max-w-3xl space-y-4">
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+                Ideite, dokumentite i proektite Vi v edno. <span className='underline'>
+                    Kaloyan's Notes
+                </span>
+            </h1>
+        </div>
+    )
+}
