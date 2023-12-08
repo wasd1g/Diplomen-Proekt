9cc603584757a15740406d7b42ad34f2105cd879
wasd1g <koki.karadjov@gmail.com>
2023-12-09 01:52:01 +0200

    navbar, footer i malko tekst i prochie

--- app/(MarketingPage)/_components/heading.tsx
+++ app/(MarketingPage)/_components/heading.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
 export const Heading = () => {
     return (
         <div className="max-w-3xl space-y-4">
@@ -8,6 +11,15 @@ export const Heading = () => {
                     Kaloyan's Notes
                 </span>
             </h1>
+            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+                Tuk mojete da si zapisvate belejki i da sledite ejednevieto si, <br />
+                po unikalen nachin.
+            </h3>
+            <Button>
+                Vlez<ArrowRight className="h-4 w-4 ml-2"/>
+                Kaloyan's Notes <br/>
+                <ArrowRight className="h-4 w-4 ml-2"/>
+            </Button>
         </div>
     )
 }
